72c1c5a0e84d8b2b473728d7790a1a0a6505131a
mcip714 <78111839+mcip714@users.noreply.github.com>
2021-02-21 18:09:15 -0700

    removing errant spaces
    again.

--- js/script.js
+++ js/script.js
@@ -124,8 +124,6 @@ The following code adds three event listeners to the page so as someone types in
 or hits enter/clicks the search submit button, the searchStudents function is called and results are displayed. 
 The third calls the clearSerchText function to clear input and return to the first page.
 */
-
-
 searchEvent.addEventListener('keyup', () => {
   searchStudents(searchInput.value);
 });
